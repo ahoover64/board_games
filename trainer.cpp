2903aecd891b29f62cf661335d1ce64bbc52ff4d
ahoover8 <alexhoover8@gmail.com>
2016-07-31 13:54:17 -0400

    slightly improved the training. i might do research into it, but i want to try and come up with my own method for teaching AI to play Amazons

--- trainer.cpp
+++ trainer.cpp
@@ -16,47 +16,59 @@ int main(int argc, char* argv[]) {
     AmazonsAI p2(2, 5, g.get_board());
     g.add_player(p1);
     g.add_player(p2);
-    int w = g.play_game();
-    //printf("Player %i is the winner\n", w);
+    int w = 0;
+
     int count = 0;
-    double* move_dummy;
-    double* shoot_dummy;
-    while (count < 100) {
+    int num_games = 1000;
+    int p1_games = 0;
+    int p2_games = 0;
+    double* p1_move = p1.move_weights();
+    double* p1_shoot = p1.shoot_weights();
+    double* p2_move = p2.move_weights();
+    double* p2_shoot = p2.shoot_weights();
+    while (count < num_games) {
         w = g.play_game();
         if (w == 1) {
-            move_dummy = p2.move_weights();
-            shoot_dummy = p2.shoot_weights();
+            p1_games++;
+            // change the loser weights to see if they can beat the winner next time
+            for (int i = 0; i < 4; i++) {
+                p2_move[i] = (p2_move[i] + p1_move[i]) / 2
+                                - (0.5 + ((double) rand() / RAND_MAX)) * (num_games - count) / 50;
+                p2_shoot[i] = (p2_shoot[i] + p1_shoot[i]) / 2
+                                - (0.5 + ((double) rand() / RAND_MAX)) * (num_games - count) / 50;
+            }
         }
         if (w == 2) {
-            move_dummy = p1.move_weights();
-            shoot_dummy = p1.shoot_weights();
-        }
-        // change the loser weights to see if they can beat the winner next time
-        for (int i = 0; i < 4; i++) {
-            move_dummy[i] = move_dummy[i] - 0.5 +((double) rand() / RAND_MAX);
-            shoot_dummy[i] = move_dummy[i] - 0.5 +((double) rand() / RAND_MAX);
+            p2_games++;
+            // change the loser weights to see if they can beat the winner next time
+            for (int i = 0; i < 4; i++) {
+                p1_move[i] = (p2_move[i] + p1_move[i]) / 2
+                                - (0.5 + ((double) rand() / RAND_MAX)) * (num_games - count) / 50;
+                p1_shoot[i] = (p2_shoot[i] + p1_shoot[i]) / 2
+                                - (0.5 + ((double) rand() / RAND_MAX)) * (num_games - count) / 50;
+            }
         }
         count++;
     }
 
-    move_dummy = p1.move_weights();
-    shoot_dummy = p1.shoot_weights();
-    printf("Player 1 move weights: %.2f, %.2f, %.2f, %.2f\n", move_dummy[0],
-                                                              move_dummy[1],
-                                                              move_dummy[2],
-                                                              move_dummy[3]);
-    printf("Player 1 shoot weights: %.2f, %.2f, %.2f, %.2f\n", shoot_dummy[0],
-                                                               shoot_dummy[1],
-                                                               shoot_dummy[2],
-                                                               shoot_dummy[3]);
-    move_dummy = p2.move_weights();
-    shoot_dummy = p2.shoot_weights();
-    printf("Player 2 move weights: %.2f, %.2f, %.2f, %.2f\n", move_dummy[0],
-                                                              move_dummy[1],
-                                                              move_dummy[2],
-                                                              move_dummy[3]);
-    printf("Player 2 shoot weights: %.2f, %.2f, %.2f, %.2f\n", shoot_dummy[0],
-                                                               shoot_dummy[1],
-                                                               shoot_dummy[2],
-                                                               shoot_dummy[3]);
+    printf("P1 won: %d / %d games\n", p1_games, num_games);
+    printf("P2 won: %d / %d games\n", p2_games, num_games);
+
+    printf("P1 move weights: %.2f, %.2f, %.2f, %.2f\n", p1_move[0],
+                                                        p1_move[1],
+                                                        p1_move[2],
+                                                        p1_move[3]);
+    printf("P1 shoot weights: %.2f, %.2f, %.2f, %.2f\n", p1_shoot[0],
+                                                         p1_shoot[1],
+                                                         p1_shoot[2],
+                                                         p1_shoot[3]);
+
+    printf("P2 move weights: %.2f, %.2f, %.2f, %.2f\n", p2_move[0],
+                                                        p2_move[1],
+                                                        p2_move[2],
+                                                        p2_move[3]);
+    printf("P2 shoot weights: %.2f, %.2f, %.2f, %.2f\n", p2_shoot[0],
+                                                         p2_shoot[1],
+                                                         p2_shoot[2],
+                                                         p2_shoot[3]);
 }
